1b62fd2105c50206fe95f18fe9d01bc246e5d4c2
zz <605364396@qq.com>
2019-03-24 12:45:01 +0800

    login

--- Secondhand/app/src/main/java/com/msi_pc/secondhand/Register.java
+++ Secondhand/app/src/main/java/com/msi_pc/secondhand/Register.java
@@ -8,8 +8,10 @@ import java.io.IOException;
 
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class Register extends Activity {
@@ -17,23 +19,24 @@ public class Register extends Activity {
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
-        String url="http://192.168.31.114:8080/string";
-        OkHttpClient okHttpClient = new OkHttpClient();
-        final Request request = new Request.Builder()
-                .url(url)
-                .get()//默认就是GET请求，可以不写
-                .build();
-        Call call = okHttpClient.newCall(request);
-        call.enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                Log.d("你好", "onFailure: ");
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                Log.d("你好", "onResponse: " + response.body().string());
-            }
-        });
+//        String url="http://192.168.31.114:8080/hello";
+//        OkHttpClient okHttpClient = new OkHttpClient();
+//
+//        final Request request = new Request.Builder()
+//                .url(url)
+//                .get()//默认就是GET请求，可以不写
+//                .build();
+//        Call call = okHttpClient.newCall(request);
+//        call.enqueue(new Callback() {
+//            @Override
+//            public void onFailure(Call call, IOException e) {
+//                Log.d("你好", "onFailure: ");
+//            }
+//
+//            @Override
+//            public void onResponse(Call call, Response response) throws IOException {
+//                Log.d("你好", "onResponse: " + response.body().string());
+//            }
+//        });
     }
 }
