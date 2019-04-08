704bd45e7913ccdb80a7f00f6fce536f0a98fd26
zz <605364396@qq.com>
2019-04-08 19:25:42 +0800

    wwe

---
+++ Secondhand/app/src/main/java/com/msi_pc/secondhand/FragmentList.java
@@ -0,0 +1,23 @@
+package com.msi_pc.secondhand;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * @Auther: msi-pc
+ * @Date: 2019/4/8 18:43
+ * @Description:
+ */
+public class FragmentList extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_list,null);
+        return view;
+    }
+}
---
+++ Secondhand/app/src/main/java/com/msi_pc/secondhand/MyFragmentAdapter.java
@@ -0,0 +1,30 @@
+package com.msi_pc.secondhand;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+
+public class MyFragmentAdapter extends FragmentPagerAdapter
+
+    {
+        List<Fragment> list;
+        public MyFragmentAdapter(FragmentManager fm,List<Fragment> list) {
+        super(fm);
+        this.list=list;
+    }//写构造方法，方便赋值调用
+        @Override
+        public Fragment getItem(int arg0) {
+        return list.get(arg0);
+    }//根据Item的位置返回对应位置的Fragment，绑定item和Fragment
+
+        @Override
+        public int getCount() {
+        return list.size();
+    }//设置Item的数量
+
+    }
+
+
