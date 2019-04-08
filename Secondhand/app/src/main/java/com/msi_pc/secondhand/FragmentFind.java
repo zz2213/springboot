0fbb4667e5d8bf6a4e729aabd2dff5de1db8e2fd
zz <605364396@qq.com>
2019-04-08 21:06:32 +0800

    商品页面

--- Secondhand/app/src/main/java/com/msi_pc/secondhand/FragmentFind.java
+++ Secondhand/app/src/main/java/com/msi_pc/secondhand/FragmentFind.java
@@ -1,5 +1,6 @@
 package com.msi_pc.secondhand;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,6 +8,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 /**
  * @Auther: msi-pc
@@ -19,5 +22,18 @@ public class FragmentFind extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_find,null);
         return view;
+    }
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+
+        super.onActivityCreated(savedInstanceState);
+        Button button = getActivity().findViewById(R.id.buy_want);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),GoodsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ Secondhand/app/src/main/java/com/msi_pc/secondhand/GoodsActivity.java
@@ -0,0 +1,18 @@
+package com.msi_pc.secondhand;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * @Auther: msi-pc
+ * @Date: 2019/4/8 20:48
+ * @Description:
+ */
+public class GoodsActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goods);
+    }
+}
