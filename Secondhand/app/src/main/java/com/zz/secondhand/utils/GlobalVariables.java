8294180d96157ecadb02a6bd69cba4c657678836
zz2213 <605364396@qq.com>
2019-04-15 17:21:25 +0800

    注册 界面

---
+++ Secondhand/app/src/main/java/com/zz/secondhand/utils/GlobalVariables.java
@@ -0,0 +1,14 @@
+package com.zz.secondhand.utils;
+
+import java.net.URL;
+
+/**
+ * @author Administrator
+ * @title: GlobalVariables
+ * @projectName Secondhand
+ * @description: TODO
+ * @date 2019/4/1515:43
+ */
+public class GlobalVariables {
+    final static public String REGISTER_URL = "http://192.168.40.116:8080//Secondhand/register";
+}
